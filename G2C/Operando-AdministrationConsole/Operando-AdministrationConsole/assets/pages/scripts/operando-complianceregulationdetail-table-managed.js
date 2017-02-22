b6fcddf1e33dddb890c4226f2e66575e91bd8e15
fabioneri <fabio.neri@progettidiimpresa.it>
2017-02-22 16:24:04 +0100

    Edit compliance report

--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/assets/pages/scripts/operando-complianceregulationdetail-table-managed.js
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/assets/pages/scripts/operando-complianceregulationdetail-table-managed.js
@@ -39,8 +39,8 @@
             //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
 
             "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
-
             "responsive": true,
+            "bFilter": false,
 
             /* Results in:
                 <div>
@@ -137,8 +137,8 @@
             //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
 
             "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
-
             "responsive": true,
+            "bFilter": false,
 
             /* Results in:
                 <div>
