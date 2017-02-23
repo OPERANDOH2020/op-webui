84fd4d4a27663cdd1b2b4635675030366521fcc0
Rob Bell <robert.bell@oxfordcc.co.uk>
2017-02-23 16:17:51 +0000

    OFUSE-61 * Adding file missed from last commit

---
+++ G2C/Operando-AdministrationConsole/eu.operando.core.bda/Model/ExtractionRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace eu.operando.core.bda.Model
+{
+    public class ExtractionRequest
+    {
+        public string RequesterName { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public string RequestSummary { get; set; }
+
+    }
+}
