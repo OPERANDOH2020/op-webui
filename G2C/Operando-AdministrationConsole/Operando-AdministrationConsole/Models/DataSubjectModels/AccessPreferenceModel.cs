59b449970e8b408c74ead0bd593d52e780bf1905
William Frankish <william.frankish@oxfordcc.co.uk>
2017-06-28 12:25:41 +0100

    OFUSE-405 replace roles with nice strings

--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/AccessPreferenceModel.cs
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/AccessPreferenceModel.cs
@@ -4,6 +4,7 @@ using eu.operando.core.pdb.cli.Model;
 
 namespace Operando_AdministrationConsole.Models.DataSubjectModels
 {
+
     public class AccessPreferenceModel
     {
         public bool Selected { get; set; }
@@ -21,30 +22,46 @@ namespace Operando_AdministrationConsole.Models.DataSubjectModels
 
     public class GroupedPolicyModel
     {
+        private static readonly Dictionary<string, string> AccessorDictionary = new Dictionary<string, string>
+        {
+            {"volunteer_linkup", "Volunteer Linkup"},
+            {"abingdon_good_neighbour_scheme", "Abingdon Good Neighbour Scheme"}
+        };
+
         public List<AccessPolicyModel> GroupedPolicies { get; set; }
-        public string GroupKey { get; set; }
+
+        public string GroupKey => AccessorDictionary.ContainsKey(RawGroupKey) ? AccessorDictionary[RawGroupKey] : RawGroupKey;
+
+        private string RawGroupKey { get; set; }
 
         public GroupedPolicyModel(GroupAccessPolicies entity)
         {
             GroupedPolicies = entity.gap.Select(ap => new AccessPolicyModel(ap)).ToList();
-            GroupKey = entity.groupKey;
+            RawGroupKey = entity.groupKey;
         }
     }
 
     public class AccessPolicyModel
     {
+        public static readonly Dictionary<string, string> ResourceDictionary = new Dictionary<string, string>
+        {
+            // TODO if we're changing these
+        };
+
         public string Subject { get; set; }
-        public string Resource { get; set; }
+        public string RawResource { get; set; }
         public bool? Permission { get; set; }
         public List<AttributeModel> Attributes { get; set; }
         public AccessPolicy.ActionEnum? Action { get; set; }
+
+        public string Resource => ResourceDictionary.ContainsKey(RawResource) ? ResourceDictionary[RawResource] : RawResource;
 
         public AccessPolicyModel(AccessPolicy ap)
         {
             Action = ap.Action;
             Attributes = ap.Attributes.Select(a => new AttributeModel(a)).ToList();
             Permission = ap.Permission;
-            Resource = ap.Resource;
+            RawResource = ap.Resource;
             Subject = ap.Subject;
         }
     }
