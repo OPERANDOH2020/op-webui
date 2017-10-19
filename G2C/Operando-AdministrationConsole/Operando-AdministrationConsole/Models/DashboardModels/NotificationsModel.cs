79c912cff67358fa0322eaf1a715d115fe4e01a9
KajiaWei-occ <Kajia.Wei@oxfordcc.co.uk>
2017-10-19 10:43:12 +0100

    [OFUSE-469] Added additional column to the notifications table in order to link to the page relevant to the notification type.

--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DashboardModels/NotificationsModel.cs
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DashboardModels/NotificationsModel.cs
@@ -1,21 +1,41 @@
 ﻿using System;
+using System.Web.Mvc;
 using eu.operando.core.ldb.Model;
 
 namespace Operando_AdministrationConsole.Models.DashboardModels
 {
     public class NotificationsModel
     {
+        private const string PrivacySettingsUpdatedStr = "privacy settings";
+        private const string OspPolicyChangeStr = "osp privacy Policy";
+
         public string Title { get; set; }
 
         public string Description { get; set; }
 
         public DateTime TimeStamp { get; set; }
 
-        public NotificationsModel(DataAccessLog log)
+        /// <summary>
+        /// The string representing the URL to go to under the action column.
+        /// </summary>
+        public string ActionUrl { get; set; }
+
+        public NotificationsModel(DataAccessLog log, UrlHelper urlHelper)
         {
             Title = log.title;
             Description = log.description;
             TimeStamp = log.logDate;
+            ActionUrl = string.Empty;
+
+            if (Title.StartsWith(PrivacySettingsUpdatedStr, StringComparison.InvariantCultureIgnoreCase))
+            {
+                ActionUrl = urlHelper.Action("AccessPreferences", "DataSubject");
+                
+            }
+            else if (Title.StartsWith(OspPolicyChangeStr, StringComparison.InvariantCultureIgnoreCase))
+            {
+                ActionUrl = urlHelper.Action("PrivacyPolicy", "OspAdmin");
+            }
         }
     }
 }
