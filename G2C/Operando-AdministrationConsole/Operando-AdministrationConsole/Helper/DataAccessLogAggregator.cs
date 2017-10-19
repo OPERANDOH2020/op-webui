83948a695599f0b8c156f8be352385183e49c8d8
William Frankish <william.frankish@oxfordcc.co.uk>
2017-10-19 16:38:04 +0100

    OF-451 aggregate logs

--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Helper/DataAccessLogAggregator.cs
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Helper/DataAccessLogAggregator.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using Operando_AdministrationConsole.Models.DataSubjectModels;
 
 namespace Operando_AdministrationConsole.Helper
@@ -10,7 +8,65 @@ namespace Operando_AdministrationConsole.Helper
     {
         public IEnumerable<DataAccessLogModel> Aggregate(IEnumerable<DataAccessLogModel> models)
         {
-            throw new NotImplementedException();
+            var groupedByRequester = models.GroupBy(m => m.RequesterId);
+            var aggregated = groupedByRequester.SelectMany(AggregateSameRequester);
+            var sorted = aggregated.OrderBy(m => m.LogDateEnd ?? m.LogDateStart);
+            return sorted;
+        }
+
+        private IEnumerable<DataAccessLogModel> AggregateSameRequester(IEnumerable<DataAccessLogModel> models)
+        {
+            var groupedByAggregatable = GroupByAggregatable(models);
+            var aggregated = groupedByAggregatable.Select(g => new DataAccessLogModel(g.ToList()));
+            return aggregated;
+        }
+
+        private IEnumerable<IList<DataAccessLogModel>> GroupByAggregatable(IEnumerable<DataAccessLogModel> models)
+        {
+            var sorted = models.OrderBy(m => m.LogDateStart).ToList();
+
+            var first = sorted.First();
+            var remaining = sorted.Skip(1);
+
+            var currentGroup = new List<DataAccessLogModel> {first};
+            var currentStart = first.LogDateStart;
+            var currentGranted = new SortedSet<string>();
+            foreach (var field in first.GrantedFields)
+            {
+                currentGranted.Add(field);
+            }
+            var currentDenied = new SortedSet<string>();
+            foreach (var field in first.DeniedFields)
+            {
+                currentDenied.Add(field);
+            }
+
+            foreach (var model in remaining)
+            {
+
+                if ((currentStart.Day != model.LogDateStart.Day) ||
+                    (model.DeniedFields.Any(f => currentGranted.Contains(f))) ||
+                    (model.GrantedFields.Any(f => currentDenied.Contains(f))))
+                {
+                    yield return currentGroup;
+                    currentStart = model.LogDateStart;
+                    currentGroup = new List<DataAccessLogModel>();
+                    currentGranted.Clear();
+                    currentDenied.Clear();
+                }
+                currentGroup.Add(model);
+                foreach (var field in model.GrantedFields)
+                {
+                    currentGranted.Add(field);
+                }
+                foreach (var field in model.DeniedFields)
+                {
+                    currentDenied.Add(field);
+                }
+
+            }
+
+            yield return currentGroup;
         }
     }
 }
--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/DataAccessLogModel.cs
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/DataAccessLogModel.cs
@@ -25,7 +25,7 @@ namespace Operando_AdministrationConsole.Models.DataSubjectModels
         public DataAccessLogModel(IList<DataAccessLogModel> models)
         {
             LogDateStart = models.Min(m => m.LogDateStart);
-            LogDateEnd = models.Max(m => m.LogDateStart);
+            LogDateEnd = models.Count > 1 ? (DateTime?) models.Max(m => m.LogDateStart) : null;
             RequesterId = models.Select(m => m.RequesterId).Distinct().Single();
             GrantedFields = models.SelectMany(m => m.GrantedFields).Distinct().ToList();
             DeniedFields = models.SelectMany(m => m.DeniedFields).Distinct().ToList();
