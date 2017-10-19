ebc0438e5c827f4f340f61273fd627e55ed28341
William Frankish <william.frankish@oxfordcc.co.uk>
2017-10-19 14:32:59 +0100

    OF-451 disregard description, format differently

--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/DataAccessLogModel.cs
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/DataAccessLogModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using eu.operando.core.ldb.Model;
 
 namespace Operando_AdministrationConsole.Models.DataSubjectModels
@@ -9,7 +8,6 @@ namespace Operando_AdministrationConsole.Models.DataSubjectModels
     public class DataAccessLogModel
     {
         public DateTime LogDate;
-        public string Description;
         public string RequesterId;
 
         public IList<string> GrantedFields;
@@ -18,7 +16,6 @@ namespace Operando_AdministrationConsole.Models.DataSubjectModels
         public DataAccessLogModel(DataAccessLog entity)
         {
             LogDate = entity.logDate;
-            Description = entity.description;
             RequesterId = entity.requesterId;
             GrantedFields = entity.arrayRequestedFields.Where(s => entity.arrayGrantedFields.Contains(s)).ToList();
             DeniedFields = entity.arrayRequestedFields.Where(s => !entity.arrayGrantedFields.Contains(s)).ToList();
