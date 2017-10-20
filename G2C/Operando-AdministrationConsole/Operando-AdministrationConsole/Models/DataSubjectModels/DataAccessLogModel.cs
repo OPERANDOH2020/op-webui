faa9ccdfaddafaef512ea96f49b19b3bfd3fcc19
William Frankish <william.frankish@oxfordcc.co.uk>
2017-10-20 17:16:46 +0100

    OF-451 add equality method for tests

--- G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/DataAccessLogModel.cs
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/Models/DataSubjectModels/DataAccessLogModel.cs
@@ -37,5 +37,35 @@ namespace Operando_AdministrationConsole.Models.DataSubjectModels
             GrantedFields = models.SelectMany(m => m.GrantedFields).Distinct().ToList();
             DeniedFields = models.SelectMany(m => m.DeniedFields).Distinct().ToList();
         }
+
+        public override bool Equals(object that)
+        {
+            var model = that as DataAccessLogModel;
+            return model != null && Equals(model);
+        }
+
+        protected bool Equals(DataAccessLogModel other)
+        {
+            return LogDateStart.Equals(other.LogDateStart) &&
+                   LogDateEnd.Equals(other.LogDateEnd) &&
+                   string.Equals(RequesterId, other.RequesterId) &&
+                   GrantedFields.Count == other.GrantedFields.Count &&
+                   GrantedFields.Zip(other.GrantedFields, string.Equals).All(b => b) &&
+                   DeniedFields.Count == other.DeniedFields.Count &&
+                   DeniedFields.Zip(other.DeniedFields, string.Equals).All(b => b);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = LogDateStart.GetHashCode();
+                hashCode = (hashCode * 397) ^ LogDateEnd.GetHashCode();
+                hashCode = (hashCode * 397) ^ (RequesterId != null ? RequesterId.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (GrantedFields != null ? GrantedFields.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (DeniedFields != null ? DeniedFields.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
