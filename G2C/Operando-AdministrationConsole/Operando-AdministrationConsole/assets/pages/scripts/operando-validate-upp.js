818d18855204bd0bfcf4661d6657c80c7788844a
panositi <pm@it-innovation.soton.ac.uk>
2017-12-19 16:53:45 +0000

    customising validator part I

---
+++ G2C/Operando-AdministrationConsole/Operando-AdministrationConsole/assets/pages/scripts/operando-validate-upp.js
@@ -0,0 +1,3 @@
+﻿jQuery.extend(jQuery.validator.messages, {
+    required: "If this field is not checked, then you cannot use the service. You will not be subscribed ot the service and your data will not be accessible."
+});
